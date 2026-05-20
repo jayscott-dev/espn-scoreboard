75d5f43dee5291e42a557e5db6a79fbb7a16067a
Jayden Scott <jayscott.ld@gmail.com>
2026-05-20 18:21:20 -0500

    cleaning up StatLeader

--- src/nba/stat_leader.py
+++ src/nba/stat_leader.py
@@ -4,11 +4,11 @@ from typing import Optional
 @dataclass
 class StatLeader:
     stat_type: str
-    athlete_name: Optional[str] = None
-    value: Optional[int] = None
+    athlete_name: str
+    value: int
 
     @classmethod
-    def from_dict(cls, stat_leader: dict) -> "StatLeader":
+    def from_dict(cls, stat_leader: dict) -> Optional["StatLeader"]:
         stat_type = stat_leader["name"] 
         leaders = stat_leader["leaders"]
         if leaders:
@@ -16,10 +16,6 @@ class StatLeader:
                 stat_type = stat_type,
                 athlete_name = leaders[0]["athlete"]["shortName"],
                 value = leaders[0]["value"]
-            )
-        else:
-            return cls(
-                stat_type = stat_type
             )
         
     @classmethod
@@ -29,7 +25,7 @@ class StatLeader:
             if overall_leader is None:
                 overall_leader = leader 
             else:
-                if leader.value is not None and overall_leader.value is not None and leader.value > overall_leader.value:
+                if leader.value > overall_leader.value:
                     overall_leader = leader
         return overall_leader
             
