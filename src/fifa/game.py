30b8ee2ce3bea5a856fce4055fbfc4e16d63458b
Jayden Scott <jayscott.ld@gmail.com>
2026-06-15 19:35:42 -0500

    adding fifa to leagues to be displayed

---
+++ src/fifa/game.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+import utils.date as date_utils
+from typing import Self
+
+@dataclass
+class FIFAGame:
+    name: str
+    date: str
+    description: str
+    home_team: str
+    away_team: str
+    home_score: str
+    away_score: str
+    display_clock: str
+    
+    @classmethod
+    def from_dict(cls, game: dict) -> Self:
+       competitions = game.get("competitions", [])[0]
+       
+       home_team = ""
+       home_score = ""
+       away_team = ""
+       away_score = ""
+
+       teams = competitions.get("competitors", [])
+       for team in teams:
+            if team["homeAway"] == "home":
+                home_team = team["team"]["name"]
+                home_score = team["score"]
+            else:
+                away_team = team["team"]["name"]
+                away_score = team["score"]
+            
+       return cls (
+            name = game.get("name", ""),
+            date = game.get("date", ""),
+            description = game["status"]["type"]["description"],
+            home_team = home_team,
+            home_score = home_score,
+            away_team = away_team,
+            away_score = away_score,
+            display_clock = game["status"]["displayClock"]
+       ) 
+    
+    def print_game_data(self):
+        print(f"\n{game_title(self)}")
+        print(f"Time: {date_utils.convert_dt(self.date).strftime("%I:%M %p")}")
+        print(f"Score: {self.away_team} {self.away_score} - {self.home_team} {self.home_score} ({self.display_clock})")
+
+def game_title(game: FIFAGame) -> str:
+    return f"{game.name} ({game.description})"
